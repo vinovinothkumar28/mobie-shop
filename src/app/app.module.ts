0723c66859154beef084634341a7058ec8ec6a3c
vinoth <vinovinothkumar28@gmai;.com>
2020-12-24 11:09:48 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatCardModule} from '@angular/material/card';
+import { HomeComponent } from './home/home.component';
+import { MatCommonModule } from '@angular/material/core';
+import { ListProductsComponent } from './list-products/list-products.component';
+import { ViewConfigurationComponent } from './view-configuration/view-configuration.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    routingComponents,
+    ListProductsComponent,
+    ViewConfigurationComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    BrowserAnimationsModule,
+    MatGridListModule,
+    MatCardModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
